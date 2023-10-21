39f687920a726f69c3a3c465c76f5cf795e31b8e
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-10-21 22:54:12 +0530

    Update todo.component.ts

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -10,21 +10,10 @@ import { TaskService } from '../task.service';
 export class TodoComponent {
   @Input() task!: Task;
   tasks: Task[] = [];
-  newTaskName: string = '';
-  newTaskDescription: string = '';
-  isAddTaskFormVisible: boolean = false;
 
   constructor(private taskService: TaskService) {}
 
   moveTaskToInProgress(task: Task) {
     this.taskService.moveTask(task, 'todo', 'inprogress');
-  }
-  openAddTaskForm() {
-    this.isAddTaskFormVisible = true;
-  }
-
-  addTask(newTask: Task) {
-    this.taskService.addTask(newTask);
-    this.isAddTaskFormVisible = false;
   }
 }
